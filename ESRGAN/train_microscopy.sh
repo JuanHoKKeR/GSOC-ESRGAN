729d00b2ce395700084e902efb36fdf2d956ddfa
Juan Cruz <juan.cruz.useche@unillanos.edu.co>
2025-05-21 08:42:39 -0500

    Comando

--- ESRGAN/train_microscopy.sh
+++ ESRGAN/train_microscopy.sh
@@ -18,6 +18,8 @@ echo "Ejecutando entrenamiento..."
 docker exec -it esrgan-container bash -c "cd /workspace && \
     python train_microscopy_meta_info.py --hr_meta_file 'datasets/paired_meta/paired_hr_meta.txt' --lr_meta_file 'datasets/paired_meta/paired_lr_meta.txt' --model_dir '/workspace/model' --log_dir '/workspace/logs' --kimianet_weights '/workspace/model-kimianet/KimiaNetKerasWeights.h5' --batch_size 16 --wandb_name 'microscopy-training' --phase 'phase1_phase2' -v"
 
+# python train_microscopy_meta_info.py --hr_meta_file 'datasets/paired_meta/paired_hr_meta.txt' --lr_meta_file 'datasets/paired_meta/paired_lr_meta.txt' --model_dir '/workspace/model' --log_dir '/workspace/logs' --batch_size 16 --wandb_name 'microscopy-training' --phase 'phase1_phase2' -v
+
 # Usar este comando para verificar logs
 echo "Puedes verificar logs con:"
 echo "docker logs -f esrgan-container"
